b575ffef73ab781f3a5e09af8c4bb7f4560b4daa
Melvyn_Rouault <melvyn.rouault@gmail.com>
2020-05-08 00:45:35 +0200

    first vesion index page

---
+++ front/recipesmanager/components/Navbar.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="container">
+      <div>
+        <h1>Recipe's Manager</h1>
+      </div>
+      <div class="navBar">
+        <b-nav>
+          <b-nav-item><img src="~/assets/img/myRecipes.png" alt="My recipes" class="imgNav"><br> My Recipes</b-nav-item>
+          <b-nav-item><img src="~/assets/img/myAccount.png" alt="My account" class="imgNav"><br> My Account</b-nav-item>
+          <b-nav-form>
+            <b-form-input aria-label="Input" id="inputFormNav"></b-form-input>
+            <b-button type="submit" id="buttonNav"><img src="~/assets/img/search.png" alt="search" id="searchNav"></b-button>
+          </b-nav-form>
+        </b-nav>
+      </div>
+    </div>
+</template>
+
+
+<style>
+.container {
+  margin-top: 1%;
+  display: flex;
+  text-align: center;
+}
+.navBar {
+  justify-content: space-between;
+}
+.imgNav {
+  width: 50px;
+  height: 50px;
+}
+#searchNav {
+  width: 35px;
+  height: 35px;
+  background-color: white;
+}
+#buttonNav {
+  background-color: white;
+  border: none;
+}
+#inputFormNav {
+  border-radius: 20px;
+}
+</style>
--- front/recipesmanager/pages/index.vue
+++ front/recipesmanager/pages/index.vue
@@ -1,72 +1,43 @@
 <template>
-  <div class="container">
+  <div>
     <div>
-      <logo />
-      <h1 class="title">
-        recipesmanager
-      </h1>
-      <h2 class="subtitle">
-        Recipe&#39;s Manager
-      </h2>
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          class="button--green"
-        >
-          Documentation
-        </a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          class="button--grey"
-        >
-          GitHub
-        </a>
-      </div>
+      <Navbar/>
     </div>
+    <b-card-group columns class="body">
+      <RecipeCardComponent/>
+      <RecipeCardComponent/>
+      <RecipeCardComponent/>
+      <RecipeCardComponent/>
+      <RecipeCardComponent/>
+      <RecipeCardComponent/>
+      <RecipeCardComponent/>
+      <RecipeCardComponent/>
+      <RecipeCardComponent/>
+    </b-card-group>
   </div>
 </template>
 
 <script>
-import Logo from '~/components/Logo.vue'
+import Navbar from '~/components/Navbar.vue';
+import RecipeCardComponent from '~/components/RecipeCardComponent.vue';
 
 export default {
   components: {
-    Logo
+    Navbar,
+    RecipeCardComponent,
   }
 }
 </script>
 
 <style>
-.container {
-  margin: 0 auto;
-  min-height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  text-align: center;
-}
 
-.title {
-  font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
-    'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
+.ramenImg {
+  width: 200px;
+  height: 100px;
 }
-
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
-}
-
-.links {
-  padding-top: 15px;
+.body {
+  margin-top: 5%;
+  margin-left: 10%;
+  margin-right: 10%;
 }
 </style>
