9be7b07306dca6b1f4e253e04dec0db76ec9eb95
Dylan Tardif <dylan.tardif1@gmail.com>
2020-05-12 17:17:26 +0200

    Intégration myAccount

---
+++ front/recipesmanager/pages/myAccount/index.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <h1>My Account</h1>
+    <div>
+      <div class="userInfos">
+        <p>Name: </p>
+        <p></p>
+      </div>
+      <div class="userInfos">
+        <p>Email: </p>
+        <p></p>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang="scss">
+  .userInfos {
+    & > p {
+      display: inline-block;
+    }
+  }
+</style>
